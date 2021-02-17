74c8e857a5bc43522d1f39905755399fb93766cb
yuvytung <yuvytung@gmail.com>
2021-02-17 22:11:17 +0700

    update api Related to post

--- src/main/java/org/regitiny/minhshop/service/impl/PostServiceImpl.java
+++ src/main/java/org/regitiny/minhshop/service/impl/PostServiceImpl.java
@@ -1,10 +1,15 @@
 package org.regitiny.minhshop.service.impl;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
 import org.jsoup.Jsoup;
+import org.regitiny.minhshop.domain.PostDetails;
+import org.regitiny.minhshop.domain.SimplePost;
+import org.regitiny.minhshop.repository.PostDetailsRepository;
+import org.regitiny.minhshop.repository.SimplePostRepository;
 import org.regitiny.minhshop.repository.search.PostDetailsSearchRepository;
 import org.regitiny.minhshop.repository.search.SimplePostSearchRepository;
 import org.regitiny.minhshop.security.AuthoritiesConstants;
@@ -21,6 +26,7 @@ import org.regitiny.minhshop.service.mapper.SimplePostMapper;
 import org.regitiny.minhshop.web.rest.custom.model.PostModel;
 import org.regitiny.minhshop.web.rest.errors.BadRequestAlertException;
 import org.regitiny.tools.magic.constant.StringPool;
+import org.regitiny.tools.magic.exception.NhecException;
 import org.regitiny.tools.magic.utils.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,6 +40,10 @@ public class PostServiceImpl implements PostService {
 
     private final PostDetailsService postDetailsService;
 
+    private final SimplePostRepository simplePostRepository;
+
+    private final PostDetailsRepository postDetailsRepository;
+
     private final SimplePostSearchRepository simplePostSearchRepository;
 
     private final SimplePostMapper simplePostMapper;
@@ -45,6 +55,8 @@ public class PostServiceImpl implements PostService {
     public PostServiceImpl(
         SimplePostService simplePostService,
         PostDetailsService postDetailsService,
+        SimplePostRepository simplePostRepository,
+        PostDetailsRepository postDetailsRepository,
         SimplePostSearchRepository simplePostSearchRepository,
         SimplePostMapper simplePostMapper,
         PostDetailsMapper postDetailsMapper,
@@ -52,13 +64,15 @@ public class PostServiceImpl implements PostService {
     ) {
         this.simplePostService = simplePostService;
         this.postDetailsService = postDetailsService;
+        this.simplePostRepository = simplePostRepository;
+        this.postDetailsRepository = postDetailsRepository;
         this.simplePostSearchRepository = simplePostSearchRepository;
         this.simplePostMapper = simplePostMapper;
         this.postDetailsMapper = postDetailsMapper;
         this.postDetailsSearchRepository = postDetailsSearchRepository;
     }
 
-    @Override
+    //  @Override
     public void createNewPost(PostModel postModel) {
         if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.MANAGEMENT)) throw new BadRequestAlertException(
             "đéo phải quản lý thì làm gì có cái quyền upload bạn ơi",
@@ -85,6 +99,8 @@ public class PostServiceImpl implements PostService {
         String thisUser = SecurityUtils.getCurrentUserLogin().get();
         String searchField = StringPool.BLANK;
 
+        if (salePrice != null && price != null) percentSale = (float) (salePrice / price);
+
         PostDetailsDTO postDetailsDTO = new PostDetailsDTO();
 
         postDetailsDTO.setUuid(UUID.randomUUID());
@@ -100,7 +116,7 @@ public class PostServiceImpl implements PostService {
 
         log.debug("postDetailsDTO = {}", postDetailsDTO);
 
-        PostDetailsDTO resultPostDetails = postDetailsService.save(postDetailsDTO);
+        PostDetails resultPostDetails = postDetailsRepository.save(postDetailsMapper.toEntity(postDetailsDTO));
 
         log.info(resultPostDetails);
 
@@ -125,14 +141,17 @@ public class PostServiceImpl implements PostService {
         simplePostDTO.setComment(comment);
         simplePostDTO.setTypePost(typePost);
         simplePostDTO.setTypePostFilters(typePostFilterDTO);
-        simplePostDTO.setPostDetails(resultPostDetails);
+        simplePostDTO.setPostDetails(postDetailsMapper.toDto(resultPostDetails));
+        SimplePost simplePost = simplePostMapper.toEntity(simplePostDTO);
 
         log.debug("simplePostDTO = {}", simplePostDTO);
 
-        SimplePostDTO resultSimplePost = simplePostService.save(simplePostDTO);
+        Long postDetails_Id = resultPostDetails.getId();
+        postDetailsRepository.findById(postDetails_Id).ifPresent(simplePost::setPostDetails);
+        SimplePost resultSimplePost = simplePostRepository.save(simplePost);
         //      sử lý đặc biệt chỉ dành cho search
 
-        PostDetailsDTO postDetailsDTOForSearch = resultSimplePost.getPostDetails();
+        PostDetails postDetailsDTOForSearch = resultSimplePost.getPostDetails();
         String contentCleanHTML = Jsoup.parse(content).text();
         searchField = StringUtils.clean(content);
 
@@ -141,10 +160,110 @@ public class PostServiceImpl implements PostService {
         postDetailsDTOForSearch.setSearchField(searchField);
         resultSimplePost.setPostDetails(postDetailsDTOForSearch);
 
-        postDetailsSearchRepository.save(postDetailsMapper.toEntity(resultPostDetails));
-        simplePostSearchRepository.save(simplePostMapper.toEntity(resultSimplePost));
+        postDetailsSearchRepository.save(resultPostDetails);
+        simplePostSearchRepository.save(resultSimplePost);
         log.debug("sau khi lưu simple-post vào es : {} , {}", resultSimplePost, resultPostDetails);
 
         log.info("upload post success");
+    }
+
+    //  @Override
+    public void partialUpdatePost(PostModel postModel, Long simplePost_Id, Long posDetail_Id) {
+        SimplePostDTO simplePostDTO = new SimplePostDTO();
+        PostDetailsDTO postDetailsDTO = new PostDetailsDTO();
+        simplePostDTO.setId(simplePost_Id);
+        postDetailsDTO.setId(posDetail_Id);
+
+        String title = postModel.getTitle();
+        Double price = postModel.getPrice();
+        Double salePrice = postModel.getSalePrice();
+        Float percentSale = postModel.getPercentSale();
+        String imageUrl = postModel.getImageUrl();
+        Float scores = postModel.getScores();
+        String simpleContent = postModel.getSimpleContent();
+        String otherInfo = postModel.getOtherInfo();
+        String content = postModel.getContent();
+        String postDetailsId = postModel.getPostDetailsId();
+        String comment = postModel.getComment();
+        Instant nowTime = Instant.now();
+        String thisUser = SecurityUtils.getCurrentUserLogin().get();
+        TypePostDTO typePost = postModel.getTypePost();
+        Set<TypePostFilterDTO> typePostFilterDTO = postModel.getTypePostFilters();
+
+        String searchField = StringPool.BLANK;
+
+        if (salePrice != null && price != null) percentSale = (float) (salePrice / price); else throw new NhecException(
+            "bạn cần phải xem lại giá bán , giảm giá"
+        );
+
+        postDetailsDTO.setPostDetailsId(postDetailsId);
+        postDetailsDTO.setContent(content);
+        postDetailsDTO.setModifiedDate(nowTime);
+        postDetailsDTO.setModifiedBy(thisUser);
+        postDetailsDTO.setDataSize((long) content.getBytes().length);
+        postDetailsDTO.setComment(comment);
+
+        Optional<PostDetailsDTO> resultPostDetails = postDetailsService.partialUpdate(postDetailsDTO);
+        if (resultPostDetails.isEmpty()) throw new NhecException();
+
+        simplePostDTO.setTitle(title);
+        simplePostDTO.setPrice(price);
+        simplePostDTO.setSalePrice(salePrice);
+        simplePostDTO.setPercentSale(percentSale);
+        simplePostDTO.setImageUrl(imageUrl);
+        simplePostDTO.setScores(scores);
+        simplePostDTO.setSimpleContent(simpleContent);
+        simplePostDTO.setOtherInfo(otherInfo);
+        simplePostDTO.setSearchField(searchField);
+        simplePostDTO.setModifiedDate(nowTime);
+        simplePostDTO.setModifiedBy(thisUser);
+        simplePostDTO.setDataSize((long) simpleContent.getBytes().length);
+        simplePostDTO.setComment(comment);
+        simplePostDTO.setTypePost(typePost);
+        simplePostDTO.setTypePostFilters(typePostFilterDTO);
+        simplePostDTO.setPostDetails(resultPostDetails.get());
+
+        if (simplePostService.partialUpdate(simplePostDTO).isEmpty()) throw new NhecException(); else log.info(
+            "update simplePost and PostDetails succeed."
+        );
+    }
+
+    @Override
+    public void save(Long simplifiedPostId, PostModel postModel) {
+        if (
+            !SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.MANAGEMENT) || SecurityUtils.getCurrentUserLogin().isEmpty()
+        ) throw new BadRequestAlertException("đéo phải quản lý thì làm gì có cái quyền upload bạn ơi", null, "notManagement");
+        Optional<SimplePostDTO> optionalSimplePostDTO = simplePostService.findOne(simplifiedPostId);
+        if (simplifiedPostId == null || optionalSimplePostDTO.isEmpty()) {
+            createNewPost(postModel);
+        } else {
+            SimplePostDTO oldSimplePostDTO = optionalSimplePostDTO.get();
+            long simplePostDTO_ID = oldSimplePostDTO.getId();
+            Optional<PostDetailsDTO> optionalPostDetailsDTO = postDetailsService.findOne(simplePostDTO_ID);
+            if (optionalPostDetailsDTO.isPresent()) {
+                PostDetailsDTO oldPostDetailsDTO = optionalPostDetailsDTO.get();
+                partialUpdatePost(postModel, oldSimplePostDTO.getId(), oldPostDetailsDTO.getId());
+            }
+            log.warn("cần xử lý trong tương lai");
+        }
+    }
+
+    @Override
+    public void deletePost(long simplePostId) {
+        simplePostService
+            .findOne(simplePostId)
+            .map(
+                simplePostDTO -> {
+                    if (simplePostDTO.getPostDetails() != null) {
+                        Long postDetailsId = simplePostDTO.getPostDetails().getId();
+                        if (postDetailsId != null) {
+                            simplePostService.delete(simplePostId);
+                            postDetailsService.delete(postDetailsId);
+                            log.debug("deleted succeed : simplePostId = {} , posDetailId = {}", simplePostId, postDetailsId);
+                        } else throw new NhecException("not find postDetails");
+                    } else simplePostService.delete(simplePostId);
+                    return null;
+                }
+            );
     }
 }
--- src/main/java/org/regitiny/minhshop/web/rest/custom/PostResource.java
+++ src/main/java/org/regitiny/minhshop/web/rest/custom/PostResource.java
@@ -7,8 +7,11 @@ import org.regitiny.minhshop.web.rest.custom.model.PostModel;
 import org.regitiny.minhshop.web.rest.errors.EmailAlreadyUsedException;
 import org.regitiny.minhshop.web.rest.errors.InvalidPasswordException;
 import org.regitiny.minhshop.web.rest.errors.LoginAlreadyUsedException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tech.jhipster.web.util.HeaderUtil;
 
 /**
  * REST controller for Post( SimplePost and PostDetails ).
@@ -20,21 +23,66 @@ public class PostResource {
 
     private final PostService postService;
 
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
     public PostResource(PostService postService) {
         this.postService = postService;
     }
 
     /**
-     * {@code POST  /post} : create a new post.
+     * {@code POST  /posts} : create a new post.
      *
-     * @throws InvalidPasswordException {@code 400 (Bad Request)} if the password is incorrect.
+     * @throws InvalidPasswordException  {@code 400 (Bad Request)} if the password is incorrect.
      * @throws EmailAlreadyUsedException {@code 400 (Bad Request)} if the email is already used.
      * @throws LoginAlreadyUsedException {@code 400 (Bad Request)} if the login is already used.
      */
     @PostMapping("/posts")
     @ResponseStatus(HttpStatus.CREATED)
-    public void registerAccount(@Valid @RequestBody PostModel postModel) {
+    public void createPost(@Valid @RequestBody PostModel postModel) {
         log.info(postModel);
-        postService.createNewPost(postModel);
+        postService.save(null, postModel);
     }
+
+    /**
+     * {@code PATCH  /posts/{id}} : update a post by simplePost_Id.
+     *
+     * @throws InvalidPasswordException  {@code 400 (Bad Request)} if the password is incorrect.
+     * @throws EmailAlreadyUsedException {@code 400 (Bad Request)} if the email is already used.
+     * @throws LoginAlreadyUsedException {@code 400 (Bad Request)} if the login is already used.
+     */
+    @PatchMapping("/posts/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void updatePost(@PathVariable Long id, @Valid @RequestBody PostModel postModel) {
+        log.info(postModel);
+        postService.save(id, postModel);
+    }
+
+    /**
+     * {@code POST  /posts/{id}} : delete a post by simplePost_Id.
+     *
+     * @throws InvalidPasswordException  {@code 400 (Bad Request)} if the password is incorrect.
+     * @throws EmailAlreadyUsedException {@code 400 (Bad Request)} if the email is already used.
+     * @throws LoginAlreadyUsedException {@code 400 (Bad Request)} if the login is already used.
+     */
+    @DeleteMapping("/posts/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Void> registerAccount(@PathVariable Long id) {
+        log.info("simplePost Id = {}", id);
+        postService.deletePost(id);
+        return ResponseEntity.noContent().headers(HeaderUtil.createAlert(applicationName, "deleted succeed", id.toString())).build();
+    }
+    //    /**
+    //     * {@code POST  /post/{id}} : deleta a post by simplePost_Id.
+    //     *
+    //     * @throws InvalidPasswordException {@code 400 (Bad Request)} if the password is incorrect.
+    //     * @throws EmailAlreadyUsedException {@code 400 (Bad Request)} if the email is already used.
+    //     * @throws LoginAlreadyUsedException {@code 400 (Bad Request)} if the login is already used.
+    //     */
+    //    @GetMapping("/posts")
+    //    @ResponseStatus(HttpStatus.CREATED)
+    //    public ResponseEntity<> getAll() {
+    //        log.info("get all ");
+    //        postService.deletePost(id);
+    //    }
 }

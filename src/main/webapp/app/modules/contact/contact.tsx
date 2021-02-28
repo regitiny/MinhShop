06bb12b005e47b2e767002c8ba0a7f0419f33fca
Phuong <phuongphi407@gmail.com>
2021-02-28 20:27:12 +0700

    database64

--- src/main/webapp/app/modules/contact/contact.tsx
+++ src/main/webapp/app/modules/contact/contact.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {BreadcrumbsItem} from 'react-breadcrumbs-dynamic';
 
 const base_path = '/home';
 
 function Contact({children})
 {
+  const promises=[];
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file.image);
+        fileReader.onload = () => {
+          file.image.data=fileReader.result
+          resolve(file.image);
+        };
+        window.console.log(file.image)
+        fileReader.onerror = (error) => {
+          reject(error);
+        };
+    });
+  }
+  const uploadImage = (event) => {
+    const {files}=event.target;
+    const newFiles=Object.keys(files).map(i=>({image:files[i]}));
+    const base64 = newFiles.map(file=>promises.push(convertBase64(file)));
+   ;
+    Promise.all(promises)
+      .then(response =>window.console.log(response))
+  }
+
   return (
     <div>
       <BreadcrumbsItem glyph="calendar" to="/contact">
@@ -19,6 +43,9 @@ function Contact({children})
       {/*  }}*/}
       {/*/>*/}
       <div>Đây là thông tin của trang web</div>
+      <div className="d-flex justify-content-center">
+        <input type="file" onChange={(event)=>uploadImage(event)}multiple/>
+      </div>
     </div>
   );
 }
